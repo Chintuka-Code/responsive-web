1160578a5ce274fe0c7e9e0a6b78189b7c5809be
mohmad sabban <mohmadsabban123@gmail.com>
2020-03-22 23:50:03 +0530

    Start responsive css grid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ImagesComponent } from './pages/images/images.component';
+import { GridPart1Component } from './pages/grid-part1/grid-part1.component';
 
 const routes: Routes = 
 [
-  {path:'image',component:ImagesComponent}
+  {path:'image',component:ImagesComponent},
+  {path:'grid',component:GridPart1Component}
 ];
 
 @NgModule({
